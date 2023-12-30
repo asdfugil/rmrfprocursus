df7bcb4861e04b6207106c061cbb8ec120256417
Nick Chan <towinchenmi@gmail.com>
2023-12-31 02:43:48 +0800

    Initial Commit

---
+++ rmrfprocursus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rmrfprocursus
+//
+//  Created by Nick Chan on 31/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rmrfprocursus/rmrfprocursusApp.swift
@@ -0,0 +1,17 @@
+//
+//  rmrfprocursusApp.swift
+//  rmrfprocursus
+//
+//  Created by Nick Chan on 31/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct rmrfprocursusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
