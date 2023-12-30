7fa30411cb1d4425f4fe2afec945712c7514e436
Nick Chan <towinchenmi@gmail.com>
2023-12-31 04:31:00 +0800

    Add a confirmation dialog

--- rmrfprocursus/ContentView.swift
+++ rmrfprocursus/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
     @State private var message = "";
@@ -13,11 +14,22 @@ struct ContentView: View {
         VStack {
             Text(message).padding(8);
             Button(action: {
-                DispatchQueue.main.async {
-                    let unsafePointer = doit();
-                    message = String(cString: unsafePointer!);
-                    free(unsafePointer);
+                let DeleteAction = UIAlertAction(title: "Delete",
+                                     style: .default) { (action) in
+                    DispatchQueue.main.async {
+                        let unsafePointer = doit();
+                        message = String(cString: unsafePointer!);
+                        free(unsafePointer);
+                    }
                 }
+                DeleteAction.setValue(UIColor.red, forKey: "titleTextColor");
+                let CancelAction = UIAlertAction(title: "Cancel", style: .default) { (action) in }
+                let alert = UIAlertController(title: "Confirm Deletion",
+                                             message: "Do you really want to delete all jailbreak files at /var/jb?",
+                                              preferredStyle: .alert);
+                alert.addAction(DeleteAction);
+                alert.addAction(CancelAction);
+                UIApplication.shared.windows.first!.rootViewController!.present(alert, animated: true, completion: nil)
             }) {
                 Text("delete")
             }
